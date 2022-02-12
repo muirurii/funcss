ed434cc2253185127bfc24dbe7b897ea6f6b53a6
muirurii <pnkanuna@gmail.com>
2022-02-12 16:09:35 +0300

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+    content: ['./index.html', './index.css'],
+    theme: {
+        extend: {
+            colors: {
+                'green': '#008000',
+                'red': '#ff0000',
+                'black': '#000',
+            },
+            fontFamily: {
+                'poppins': ['Poppins', 'serif']
+            }
+
+        },
+    },
+    plugins: [],
+}
